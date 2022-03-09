ff91f822372ea7d3bb927c7a3c1b6b4e5233caa3
Spongey <71215126+Spongeyboi@users.noreply.github.com>
2022-03-09 08:57:32 -0500

    Fix discord

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,5 +1,6 @@
 package;
 
+#if sys
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
 
@@ -78,3 +79,4 @@ class DiscordClient
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
 }
+#end
